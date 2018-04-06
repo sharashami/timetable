2521480f81ae593ff34456184ddf3710ea31c09e
sharashami <shara.alves@gmail.com>
2018-04-06 20:20:51 -0300

    rest to get schedule details

--- api/Models/ScheduleModel.php
+++ api/Models/ScheduleModel.php
@@ -12,6 +12,23 @@ class ScheduleModel extends BaseModel
         $stmt = parent::con()->prepare($query);
         $stmt->execute([$laboratory_id, $id]);
     }
-    
+    final public function listPeriods( )
+    {
+        $stmt = parent::con()->prepare('SELECT * from period');
+        $stmt->execute();    
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 
+    final public function listWeekdays( )
+    {
+        $stmt = parent::con()->prepare('SELECT * from weekday');
+        $stmt->execute();    
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    final public function listWeekShifts( )
+    {
+        $stmt = parent::con()->prepare('SELECT * from shift');
+        $stmt->execute();    
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
--- api/Rests/ScheduleRest.php
+++ api/Rests/ScheduleRest.php
@@ -11,48 +11,36 @@ class ScheduleRest extends BaseRest
         parent::__construct();
         
         parent::add('GET', [
-            ['courses/professor/:idprofessor/semester/:idsemester/', 'list']
-        ]);
-        parent::add('POST', [
-            ['courses/:idavailablecourse/professor/:idprofessor/', 'save']
-
+            ['schedule/shifts/', 'listShifts'],
+            ['schedule/periods/', 'listWeekdays'],
+            ['schedule/weekdays/', 'listPeriods'],
         ]);
         parent::add('PUT', [
             ['courses/professor/:id/laboratory/:idlaboratory/', 'setLaboratory']
         ]);
         
-        parent::add('DELETE', [
-            ['courses/professor/:id', 'delete']
-        ]);
     }
-
-    final public function save()
-    {
-        $this->model->save(parent::getParam("idavailablecourse"),parent::getParam("idprofessor"));
-        parent::response("", 200);
-    } 
 
     final public function setLaboratory()
     {
         $this->model->setLaboratory(parent::getParam("id"),parent::getParam("idlaboratory"));
         parent::response("", 200);
     } 
-    final public function list()
+    final public function listShifts()
     {
-        $courses = $this->model->list(parent::getParam("idsemester"),parent::getParam("idprofessor"));
-        parent::response($courses, 200);
+        $r = $this->model->listShifts();
+        parent::response($r, 200);
     } 
 
-    final public function listByProfessor()
+    final public function listWeekdays()
     {
-        $courses = $this->model->listByProfessor(parent::getParam("idsemester"),parent::getParam("idprofessor"));
-        parent::response($courses, 200);
+        $r = $this->model->listWeekdays();
+        parent::response($r, 200);
     } 
-
-
-    final public function delete()
+    final public function listPeriods()
     {
-        $this->model->delete(parent::getParam("id"));
-        parent::response("", 200);
+        $r = $this->model->listPeriods();
+        parent::response($r, 200);
     } 
+ 
 }
